7e51ca0b4ead80cb86c36acb2f6ad66df8acd9ea
Adarsh P S <adu23adarsh@gmail.com>
2026-04-22 11:02:04 +0530

    CORS update

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "https://ai-interview-f.vercel.app/",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type"]
 }));
